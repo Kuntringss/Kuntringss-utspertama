8bc3c033c995dfe585d7fcfa2a22da5b8588eaf4
Kuntringss <hirayukkigamers@gmail.com>
2023-05-11 16:47:20 +0700

    uts

--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,20 @@
 package pengguna;
 
 class Mhs {
-    Mhs () {
-//ini komentar
+    String nama;
+    String nim;
+    String smt;
+
+
+ void setNama(String arg_nama){
+    this.nama = arg_nama;
     }
+
+void setNim(String arg_nim){
+    this.nim = arg_nim;
+}
+
+void setSmt(int arg_smt){
+    this.smt = arg_smt;
+}
 }
--- pengguna/User.java
+++ pengguna/User.java
@@ -2,4 +2,22 @@ package pengguna;
 
 public class User {
 
+private String username;
+private String password;
+
+public void setUsername(String username){
+    this.username = username;
+}
+
+public String getUsername(){
+    return this.username;
+}
+
+public void setPassword(String password){
+    this.password = password;
+}
+
+public string getPassword(){
+    return this.password;
+}
 }
